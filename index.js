8202533c782805871182da6165b34588befa0d34
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-04-29 13:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,51 +35,51 @@ app.use(
 app.use('/ariang', express.static(__dirname + '/ariang'))
 app.get('/', (req, res) => {
 	res.send(`
-	<!DOCTYPE html>
-	<html lang="en" >
-		<head>
-			<meta charset="utf-8" />
-			<title>Pure HTML5 file upload | Script Tutorials</title>
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-			<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-			<style>
-				#box {
-					padding-top: 20vh;
-				}
-			</style>
-		</head>
-		<body>
-		<div id ="box">
-			<div class="col d-flex justify-content-center " >
-				<div class="card border-secondary mb-3" style="max-width: 40rem;">
-					<div class="card-header">Upload Center</div>
-					<div class="card-body text-secondary">
-					   
-						<div>
-							<div><label for="image_file">User ID : </label></div>
-							<div><input name="title" id="secret" class="form-control" type="text" require></div>
-						</div>
-						<div style="padding-top: 20px;">
-							<input name="button" id="panel" type="button" value="Cpanel" class="btn btn-outline-warning" />
-							<input name="button" id="downloads" type="button" value="File Manager" class="btn btn-outline-danger" /></a>
-						</div>
-						
-					</div>
-				</div>
-			</div>
-			<script>
-			panel.onclick=function(){
-				open('/ariang/#!/settings/rpc/set/wss/'+location.hostname+'/443/jsonrpc/'+btoa(secret.value),'_blank')
-			}
-			downloads.onclick=function(){
-				open('/downloads/'+btoa(secret.value)+'/')
-			}
-			</script>
-		</div>
-		</body>
-	</html>
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="utf-8" />
+        <title>Lakshan |Torrent Downloader</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <style>
+            #box {
+                padding-top: 20vh;
+            }
+        </style>
+    </head>
+    <body>
+    <div id ="box">
+        <div class="col d-flex justify-content-center " >
+            <div class="card border-secondary mb-3" style="max-width: 40rem;">
+                <div class="card-header">Torrent Downloader Login</div>
+                <div class="card-body text-secondary">
+                   
+                    <div>
+                        <div><label for="image_file">User ID : </label></div>
+                        <div><input name="title" id="secret" class="form-control" type="text" require></div>
+                    </div>
+                    <div style="padding-top: 20px;">
+                        <input name="button" id="panel" type="button" value="Cpanel" class="btn btn-outline-warning" />
+                        <input name="button" id="downloads" type="button" value="File Manager" class="btn btn-outline-danger" /></a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <script>
+        panel.onclick=function(){
+            open('/ariang/#!/settings/rpc/set/wss/'+location.hostname+'/443/jsonrpc/'+btoa(secret.value),'_blank')
+        }
+        downloads.onclick=function(){
+            open('/downloads/'+btoa(secret.value)+'/')
+        }
+        </script>
+    </div>
+    </body>
+</html>
 `)
 })
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
