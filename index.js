dee430a891db3938cc81a3a2355cd4f6e94e206d
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-04-29 13:21:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.use(
 app.use('/ariang', express.static(__dirname + '/ariang'))
 app.get('/', (req, res) => {
 	res.send(`
-<label for="secret">Enter your aria2 secret:</label>
+<label for="secret">User ID:</label>
 <input id="secret" type="text">
-<button id="panel">Go to AriaNg panel</button>
+<button id="panel">Go to C panel</button>
 <button id="downloads">View downloaded files</button>
 <script>
 panel.onclick=function(){
